0d8ba7df9a9724326d6f0f344a75ccaca0749e6a
as25353103 <as25353103@gmail.com>
2024-03-21 11:53:10 +0800

    刪除不必要的資料

--- back-end/app.js
+++ back-end/app.js
@@ -35,60 +35,6 @@ var conn= mysql.createConnection({
     database:"jiuing"
 })
 
-app.get("/",(req, res)=> {
-     // 查詢數據庫中的數據
-     conn.query("SELECT * FROM post", {},(err, results) => {
-         // 渲染模板並將數據發送到客戶端
-         res.render("index", { data: results });
-        //  console.log(results);[{}]
-     });
-    
-})
-
-
-//登入者的貼文
-app.get("/ownpost/:id",(req,res)=>{
-    conn.query("select * from post where postId = ?",
-        [req.params.id],
-        function (err,results){
-            if (err) {
-                console.error(err);
-                return res.status(500).render("error_page500.ejs");
-            }
-    
-            // 如果找不到紀錄，導致錯誤頁面
-            if (results.length === 0) {
-                return res.status(404).render("error_page400.ejs");
-            }
-
-            // 執行第二個查詢
-            conn.query("SELECT * FROM coment WHERE com_postId = ?", [req.params.id], function(err, comentResult) {
-                if (err) {
-                    console.error(err);
-                    return res.status(500).render("error_page500.ejs");
-                }
-            
-                // 渲染模板
-                res.render("own_post_page.ejs", { 
-                    post: results[0],
-                    comments: comentResult // 將評論資料傳遞給模板
-                });
-                // console.log(results);
-                // console.log(comentResult);
-    });
-        }
-    )  
-})
-
-//登入者的會員頁面 
-app.get("/ownMember",(req,res)=>{
-    res.render("own_member");
-})
-
-//登入者的會員帳號編輯頁面 
-app.get("/ownMember/settingEdit",(req,res)=>{
-    res.render("setting_edit");
-})
 
 //導入註冊
 var register = require("./routers/register");
